71f5b9f067d930e806f9116d8fd63fc22718d6c8
J. Ky Marsh <jkymarsh@gmail.com>
2014-03-29 01:08:07 -0500

    Update gulp tasks with watchify and separate bundle for external libs

---
+++ blank-seed.js
@@ -0,0 +1,2 @@
+// this file's empty because the bundle.required() libs are dumped here during
+//	the browserify process
--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,99 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	uglify = require('gulp-uglify'),
-	browserify = require('gulp-browserify');
-
-gulp.task('dev-js', function() {
-    return gulp.src('public/js/main.js')
-        .pipe(browserify({
-        	// we have to transform the browserified version of node's 'fs' module to allow
-        	//	it to pull in file references (like .html templates and partials) in a browser
-        	//	context
-        	transform: ['brfs'],
-        	// enable sourcemaps
-        	debug: true,
-        	shim: {
-	            angular: {
-	                path: 'node_modules/angular/angular.js',
-	                exports: 'angular'
-	            },
-	            'angular-route': {
-	                path: 'node_modules/angular-route/angular-route.js',
-	                exports: 'ngRoute',
-	                depends: {
-	                    angular: 'angular'
-	                }
-	            }
-          	}
-        }))
-        // handle errors to avoid gulp crashing when a watched file has errors
-        .on('error', gutil.log)
-        .pipe(gulp.dest('public/js/built'));
-});
-
-gulp.task('prod-js', function() {
-    return gulp.src('public/js/main.js')
-        .pipe(browserify({
-        	transform: ['brfs'],
-        	debug: false,
-        	shim: {
-	            angular: {
-	                path: 'node_modules/angular/angular.min.js',
-	                exports: 'angular'
-	            },
-	            'angular-route': {
-	                path: 'node_modules/angular-route/angular-route.min.js',
-	                exports: 'ngRoute',
-	                depends: {
-	                    angular: 'angular'
-	                }
-	            }
-          	}
-        }))
-        // minify!
-        .pipe(uglify({
-        	mangle: false
-        }))
-        .pipe(gulp.dest('public/js/built'));
-});
+    rename = require("gulp-rename"),
+	browserify = require('gulp-browserify'),
+    source = require('vinyl-source-stream'),
+    watchify = require('watchify');
 
 // default task acts as a 'dev' environment, watching your code and compiling as you
-//	develop
-gulp.task('default', ['dev-js'], function() {
-    gulp.watch('public/js/main.js', ['dev-js']);
+//  develop
+gulp.task('default', function() {
+    var bundler = watchify('./public/js/main.js');
 
-    gulp.watch('public/js/app/**/*.js', ['dev-js']);
+    bundler.transform('brfs');
+
+    bundler.external('angular');
+    bundler.external('angular-route');
+        
+    bundler.on('update', rebundle);
+
+    function rebundle () {
+        console.log('Bundling dev at:', new Date());
+
+        return bundler.bundle({
+                debug: true
+            })
+            .on('error', gutil.log)
+            .pipe(source('app.js'))
+            .pipe(gulp.dest('public/js/built'));
+    }
+
+    return rebundle();
+});
+
+gulp.task('libs-dev', function() {
+    return gulp.src('blank-seed.js')
+        .pipe(browserify({
+            // enable sourcemaps
+            debug: true
+        }))
+        .on('prebundle', function(bundle) {
+            bundle.require('angular/angular.js', {
+                expose: 'angular'
+            });
+
+            bundle.require('angular-route/angular-route.js', {
+                expose: 'angular-route'
+            });
+        })
+        // handle errors to avoid gulp crashing when a watched file has errors
+        .on('error', gutil.log)
+        .pipe(rename('libs.js'))
+        .pipe(gulp.dest('public/js/built'));
+});
+
+gulp.task('libs-prod', function() {
+    return gulp.src('blank-seed.js')
+        .pipe(browserify({
+            // disable sourcemaps
+            debug: false
+        }))
+        .on('prebundle', function(bundle) {
+            bundle.require('angular/angular.min.js', {
+                expose: 'angular'
+            });
+
+            bundle.require('angular-route/angular-route.min.js', {
+                expose: 'angular-route'
+            });
+        })
+        // handle errors to avoid gulp crashing when a watched file has errors
+        .on('error', gutil.log)
+        // minify, preserving certain variables and keywords
+        .pipe(uglify({
+            mangle: false
+        }))
+        .pipe(rename('libs.js'))
+        .pipe(gulp.dest('public/js/built'));
 });
 
 // prod task acts as preparation for a production environment
-gulp.task('prod', ['prod-js']);
+gulp.task('prod', function() {
+    console.log('Bundling prod at:', new Date());
+
+    return gulp.src('public/js/main.js')
+        .pipe(browserify({
+            transform: ['brfs'],
+            debug: false,
+            external: ['angular', 'angular-route']
+        }))
+        // handle errors to avoid gulp crashing when a watched file has errors
+        .on('error', gutil.log)
+        // minify, preserving certain variables and keywords
+        .pipe(uglify({
+            mangle: false
+        }))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public/js/built'));
+});
